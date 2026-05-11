0640f3256f0b727c21404eda51b028e02956e19b
Vagahbond <vagahbond@pm.me>
2026-05-12 11:28:05 +1200

    module: remove workdir

--- nix/module.nix
+++ nix/module.nix
@@ -13,7 +13,7 @@ let
   startScript = pkgs.writeShellScriptBin "firesplit.sh" ''
     set -a
     FIREFLY_KEY=$(cat ${config.services.firefly-iii.settings.APP_KEY_FILE})
-    DATABASE_URI=postgres://${config.services.firefly-iii.settings.DB_USERNAME}/${config.services.firefly-iii.settings.DB_DATABASE}?host=/run/postgresql
+    DATABASE_URI='postgres://${config.services.firefly-iii.settings.DB_USERNAME}/${config.services.firefly-iii.settings.DB_DATABASE}?host=/run/postgresql'
     PORT=${toString cfg.port}
 
 
