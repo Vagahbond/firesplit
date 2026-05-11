2d77c11fd7f20d1106e6826c6da9524caab25078
Vagahbond <vagahbond@pm.me>
2026-05-12 11:03:46 +1200

    module: remove workdir

--- nix/module.nix
+++ nix/module.nix
@@ -62,7 +62,6 @@ in
         ExecStart = "${startScript}/bin/firesplit.sh";
         WorkingDirectory = self.packages.${pkgs.stdenv.system}.default;
         User = config.services.firefly-iii.user;
-        Group = config.services.firefly-iii.user;
         # StateDirectory = "firefly-iii";
         # ReadWritePaths = [
         #   config.services.firefly-iii.dataDir
