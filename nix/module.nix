5413c279e870da2cf7981ecd7b4d30bbfe47a534
Vagahbond <vagahbond@pm.me>
2026-05-13 11:37:03 +1200

    firefly-iii: add button for firesplit

--- nix/module.nix
+++ nix/module.nix
@@ -45,13 +45,11 @@ in
   config = lib.mkIf cfg.enable {
     services.nginx.virtualHosts = lib.mkIf config.services.firefly-iii.enableNginx {
       ${config.services.firefly-iii.virtualHost} = {
+        extraConfig = ''
+          sub_filter_once on;
+          sub_filter '<ul class="sidebar-menu tree" data-widget="tree">' '<ul class="sidebar-menu tree" data-widget="tree"><li><a href="https://money.vagahbond.com/split" class="logout-link"><em class="fa fa-code-fork fa-fw"></em><span>Firesplit</span></a></li>';
+        '';
         locations = {
-          "/" = {
-            extraConfig = ''
-              sub_filter_once on;
-              sub_filter '<ul class="sidebar-menu tree" data-widget="tree">' '<ul class="sidebar-menu tree" data-widget="tree"><li><a href="https://money.vagahbond.com/split" class="logout-link"><em class="fa fa-code-fork fa-fw"></em><span>Firesplit</span></a></li>';
-          '';
-
           "/split/" = {
             proxyPass = "http://127.0.0.1:${toString cfg.port}/";
             proxyWebsockets = true;
