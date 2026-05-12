f2a7f4c4786db474112416e893b322e2b3e92340
Vagahbond <vagahbond@pm.me>
2026-05-12 14:41:16 +1200

    module: fix nginx redirection

--- nix/module.nix
+++ nix/module.nix
@@ -47,6 +47,10 @@ in
               proxy_set_header X-Forwarded-Prefix /split;
               proxy_set_header Host $host;
               proxy_set_header X-Real-IP $remote_addr;
+
+              sub_filter_once on;
+              sub_filter 'href="/' 'href="/split/';
+              sub_filter 'src="/' 'src="/split/';
             '';
           };
         };
